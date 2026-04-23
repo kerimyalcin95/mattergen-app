054d9a4ce9f282fcd8c3edaa1ebaa193ce49921b
Kerim Yalcin <kerimyalcin.95@it-yalc.in>
2026-04-23 10:47:50 +0200

    chore: split windows and linux support onto seperate files

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import win32
+
 import tkinter as tk
 import subprocess
 import sys
@@ -5,51 +7,8 @@ from pathlib import Path
 import json
 
 if sys.platform == "win32":
-    print("Running on Windows (experimental)")
-    ps = subprocess.Popen(
-    ["powershell", "-NoExit"],
-    stdin=subprocess.PIPE,
-    stdout=None,
-    stderr=None,
-    text=True,
-    creationflags=subprocess.CREATE_NEW_CONSOLE
-
-    root = tk.Tk()
-    root.title("MatterGen-App")
-    root.geometry("800x600")
-
-    # Load config file
-    BASE_DIR = Path(__file__).resolve().parent 
-    CONFIG_PATH = BASE_DIR.parent / "mattergen-app" / "config.json"
-
-    with open(CONFIG_PATH, "r") as f:
-        config = json.load(f)
-
-    label = tk.Label(root, text="mattergen-generate")
-    label.pack()
-
-    # Run mattergen CLI prompt
-    def shellProc():
-        ps.stdin.write(r'cd "' + config["work-path"] + '"' + "\n")
-        ps.stdin.write(r'.\.venv\Scripts\activate' + "\n")
-        ps.stdin.write(r'mattergen-generate "' + config["result-path"] + '" --pretrained-name=mattergen_base --batch_size=' + str(config["batch-size"]) +  r' --num_batches=' + str(config["num-batches"]) + "\n")
-        ps.stdin.flush()
-
-    # Closing the window kills all processes immediately
-    def on_close():
-        try:
-            subprocess.run(f"taskkill /F /T /PID {ps.pid}", shell=True)
-        except Exception:
-            pass
-        root.destroy()
-
-    button = tk.Button(root, text="Run")
-    button.config(command=shellProc)
-    button.pack()
-
-    root.protocol("WM_DELETE_WINDOW", on_close)
-    root.mainloop()
-)
+    # experimental
+    main_win32()
 elif sys.platform == "linux":
     print("Running on Linux distribution.")
 else:
