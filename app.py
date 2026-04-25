56e4c933b63d29ee7535a2b74efea56ca2a8f3a1
Kerim Yalcin <kerimyalcin.95@it-yalc.in>
2026-04-25 19:29:06 +0200

    feat: implement num_batches and batch_size

--- app.py
+++ app.py
@@ -19,7 +19,11 @@ class App:
         self.generate_command = None
         self.BASE_DIR = None
         self.CONFIG_PATH = None
-        self.selected_model = None
+
+        self.tkVar_options_selected_model = None
+        self.tkVar_num_batches = None
+        self.tkVar_batch_size = None
+
         self.process = None
         self.thread = None
 
@@ -44,28 +48,68 @@ class App:
         with open(self.CONFIG_PATH, "r") as f:
             self.config = json.load(f)
 
-        options_internalModels = self.config["internal-models"]
-        selected_model = tk.StringVar()
-        selected_model.set(self.config["internal-model-selected"])
+        def validate_positive_int(value):
+            return value.isdigit() or value == ""
+
+
+        tkLabel_internal_models = tk.Label(self.root, text="Available internal models:")
+        tkLabel_internal_models.pack()
+
+        options_internal_models = self.config["internal-models"]
+        self.tkVar_selected_model = tk.StringVar()
+        self.tkVar_selected_model.set(self.config["internal-model-selected"])
         tkDropDownMenu_internalModels = ttk.Combobox(
             self.root,
-            textvariable=selected_model,
-            values=options_internalModels,
+            textvariable=self.tkVar_selected_model,
+            values=options_internal_models,
             state="readonly"
         )
         tkDropDownMenu_internalModels.pack()
-
         tkDropDownMenu_internalModels.bind("<<ComboboxSelected>>", self.tkDropDownMenu_internalModels_on_select)
-        label = tk.Label(self.root, text="Generate crystal structures")
-        label.pack()
+        
 
-        button = tk.Button(self.root, text="RUN")
-        button.config(command=self.start_thread)
-        button.pack()
+        tkLabel_num_batches = tk.Label(self.root, text="Number of batches:")
+        tkLabel_num_batches.pack()
 
-        button = tk.Button(self.root, text="STOP")
-        button.config(command=self.stop_shell_process)
-        button.pack()
+        self.tkVar_num_batches = tk.StringVar()
+        self.tkVar_num_batches.set(self.config["num-batches"])
+        self.tkVar_num_batches.trace_add("write", self.tkEntry_num_batches_callback)
+        
+        self.tkEntry_num_batches = tk.Entry(
+            self.root,
+            textvariable=self.tkVar_num_batches,
+            validate="key",
+            validatecommand=(self.root.register(validate_positive_int), "%P"))
+        self.tkEntry_num_batches.bind("<KeyRelease>", self.tkEntry_num_batches_on_keyrelease)
+        self.tkEntry_num_batches.pack()
+
+
+        tkLabel_batch_size = tk.Label(self.root, text="Batch size:")
+        tkLabel_batch_size.pack()
+
+        self.tkVar_batch_size = tk.StringVar()
+        self.tkVar_batch_size.set(self.config["batch-size"])
+        self.tkVar_batch_size.trace_add("write", self.tkEntry_batch_size_callback)
+        
+        self.tkEntry_batch_size = tk.Entry(
+            self.root,
+            textvariable=self.tkVar_batch_size,
+            validate="key",
+            validatecommand=(self.root.register(validate_positive_int), "%P"))
+        self.tkEntry_batch_size.bind("<KeyRelease>", self.tkEntry_batch_size_on_keyrelease)
+        self.tkEntry_batch_size.pack()
+        
+
+        tkLabel_Process = tk.Label(self.root, text="Generate crystal structures")
+        tkLabel_Process.pack()
+
+        tkButton_Run = tk.Button(self.root, text="RUN")
+        tkButton_Run.config(command=self.start_thread)
+        tkButton_Run.pack()
+
+        tkButton_Stop = tk.Button(self.root, text="STOP")
+        tkButton_Stop.config(command=self.stop_shell_process)
+        tkButton_Stop.pack()
 
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
         self.root.mainloop()
@@ -118,8 +162,38 @@ class App:
         self.root.destroy()
 
     def tkDropDownMenu_internalModels_on_select(self, event):
-        self.config["internal-model-selected"] = self.selected_model.get()
+        self.config["internal-model-selected"] = self.tkOptions_selected_model.get()
         self.save_config()
+
+    def tkEntry_num_batches_on_keyrelease(self, event):
+        self.save_config()
+
+    def tkEntry_num_batches_callback(self, *args):
+            value = self.tkVar_num_batches.get()
+
+            if value.isdigit():
+                self.config["num-batches"] = int(value)
+                if int(value) == 0:
+                    self.config["num-batches"] = 1
+            else:
+                self.config["num-batches"] = 1
+
+            self.save_config
+
+    def tkEntry_batch_size_on_keyrelease(self, event):
+        self.save_config()
+
+    def tkEntry_batch_size_callback(self, *args):
+            value = self.tkVar_batch_size.get()
+
+            if value.isdigit():
+                self.config["batch-size"] = int(value)
+                if int(value) == 0:
+                    self.config["batch-size"] = 1
+            else:
+                self.config["batch-size"] = 1
+
+            self.save_config
 
 if __name__ == "__main__":
     app = App()
